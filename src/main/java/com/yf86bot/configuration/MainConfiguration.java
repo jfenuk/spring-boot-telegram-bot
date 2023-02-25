02ccc0a47bbf8162a55e678b850d1fbdbad619af
Evgeniy Fenuk <efen@ciklum.com>
2023-02-25 19:26:52 +0100

    Github Actions integration.

--- src/main/java/com/yf86bot/configuration/MainConfiguration.java
+++ src/main/java/com/yf86bot/configuration/MainConfiguration.java
@@ -1,17 +1,19 @@
 package com.yf86bot.configuration;
 
 import com.yf86bot.controller.MainController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MainConfiguration {
 
-    private static final String TOKEN = "6067961010:AAEHjkhTKK14S7IVQEGwzRn7QsM_fRInp9A";
+    @Value("${TELEGRAM_TOKEN}")
+    private String telegramToken;
 
     @Bean
     public MainController mainController() {
-        return new MainController(TOKEN);
+        return new MainController(telegramToken);
     }
 
 }
