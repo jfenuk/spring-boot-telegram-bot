6c59d9a6752ce70d9fb9695d96937e7b3b745a0d
Evgeniy Fenuk <efen@ciklum.com>
2023-02-25 18:13:40 +0100

    Github Actions integration.

--- src/main/java/com/yf86bot/controller/MainController.java
+++ src/main/java/com/yf86bot/controller/MainController.java
@@ -1,12 +1,16 @@
 package com.yf86bot.controller;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MainController extends TelegramLongPollingBot {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
     private static final String BOT_NAME = "YF86_bot";
 
@@ -26,6 +30,7 @@ public class MainController extends TelegramLongPollingBot {
             var msg = update.getMessage();
             var chatId = msg.getChatId();
             try {
+                LOGGER.info("Incoming message: {}", msg.getText());
                 var reply = msg.getText().contains("Glory to Ukraine") ? "Glory to the heroes" : "Glory to Ukraine";
                 sendNotification(String.valueOf(chatId), reply);
             } catch (TelegramApiException e) {
