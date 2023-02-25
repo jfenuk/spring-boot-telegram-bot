336eb02e48e0c0ef12d93e458c36818c5e980414
Evgeniy Fenuk <efen@ciklum.com>
2023-02-25 22:52:00 +0100

    introduce application properties file.

--- src/main/java/com/yf86bot/configuration/MainConfiguration.java
+++ src/main/java/com/yf86bot/configuration/MainConfiguration.java
@@ -1,6 +1,8 @@
 package com.yf86bot.configuration;
 
 import com.yf86bot.controller.MainController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MainConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainConfiguration.class);
     @Value("${telegram.token}")
     private String telegramToken;
 
     @Bean
     public MainController mainController() {
+        LOGGER.info("Telegram token value: {}", telegramToken);
         return new MainController(telegramToken);
     }
 
